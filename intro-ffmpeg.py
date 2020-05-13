6aaa60a8c8fbd14042c8c172875d80b2e090bc53
Vasu Gupta <guptavasu1213@gmail.com>
2020-05-13 01:44:47 -0600

    Split video into chunks with re-encoding

--- intro-ffmpeg.py
+++ intro-ffmpeg.py
@@ -31,21 +31,20 @@ def get_video_length(input_file):
 
 def split_video_into_chunks(input_stream, input_file, output_file, seconds):
 	"""
-	Splits the input stream into multiple files
+	Splits the input stream into multiple files with re-encoding
 	:param input_stream: input file stream
+	:param input_file: input file name
 	:param output_file: output file name
 	:param seconds: the duration of each splitted video (in seconds)
 	"""
 	folder_name = input_file.split(".")[0]
-	subprocess.run("mkdir " + folder_name, shell=True)
+	subprocess.run("mkdir " + folder_name, shell=True) # Creating a folder with the same name as the input video
 
 	output, extension = output_file.split(".")	
-	
-	start_seconds = 20
-	
+		
 	length_in_seconds = get_video_length(input_file)
 	total_videos_to_create = (int(length_in_seconds)//seconds) + 1
-	i = 4
+
 	# cmd = "ffmpeg -ss " + str(start_seconds) + " -i " + input_file + " -c copy"+ " -t " + str(length_in_seconds-start_seconds) + \
 	# " " + folder_name + "/" +output+str(i+1)+"."+extension
 	# subprocess.run(cmd, shell=True)
@@ -53,16 +52,27 @@ def split_video_into_chunks(input_stream, input_file, output_file, seconds):
 	
 	#############Works but inaccurate
 	#ffmpeg -i bridge.mp4 -ss 20 -t 4.0300375469336664 -c copy bridge/output_file5.mp4
-	cmd = "ffmpeg -i " + input_file + " -ss " + str(start_seconds) + " -t " + str(length_in_seconds-start_seconds) + \
-	" -c:v copy -an " + folder_name + "/" +output+str(i+1)+"."+extension
-	subprocess.run(cmd, shell=True)
-	print(cmd)
-	i=0
-	# return
+	# # cmd = "ffmpeg -i " + input_file + " -ss " + str(start_seconds) + " -t " + str(length_in_seconds-start_seconds) + \
+	# # " -c:v copy -an " + folder_name + "/" +output+str(i+1)+"."+extension
+	# subprocess.run(cmd, shell=True)
+	# print(cmd)
 	for i in range(total_videos_to_create-1):
 		start_seconds = (i*seconds)
-		print(start_seconds)
-		# i =0
+
+		# ffmpeg -ss <starting point> -i <input file> -t <duration of the clip> <output file>
+		cmd = "ffmpeg -ss " + str(start_seconds) + " -i " + input_file + " -t " + str(seconds) + \
+			" -y " + folder_name + "/" + output + str(i+1) + "." +extension
+		subprocess.run(cmd, shell=True)
+		print(cmd)
+
+	# For the last video:
+	start_seconds = (total_videos_to_create-1) * seconds
+	cmd = "ffmpeg -ss " + str(start_seconds) + " -i " + input_file + " -t " + str(length_in_seconds-start_seconds) + \
+	" -y " + folder_name + "/" +output+str(total_videos_to_create)+"."+extension
+	subprocess.run(cmd, shell=True)
+	print(cmd)
+
+		# OTHER WAY
 		# (
 		# 	input_stream
 		# 		.filter("trim", start=start_seconds, duration=seconds)
@@ -73,14 +83,13 @@ def split_video_into_chunks(input_stream, input_file, output_file, seconds):
 		# Example: ffmpeg -ss 0 -i bridge.mp4 -t 5 -vcodec copy bridge/output_file1.mp4
 
 		##########Not working
-		cmd = "ffmpeg -ss " + str(start_seconds) + " -i " + input_file + " -t " + str(seconds) + \
-		" -vcodec copy " + folder_name + "/" +output+str(i+1)+"."+extension
+		# cmd = "ffmpeg -ss " + str(start_seconds) + " -i " + input_file + " -t " + str(seconds) + \
+		# " -vcodec copy " + folder_name + "/" +output+str(i+1)+"."+extension
 
+		# NOt working with the same encoding
 		# cmd = "ffmpeg -i " + input_file + " -ss " + str(start_seconds) + " -to " + str(start_seconds+seconds) + \
 		# " -vcodec copy -an " + folder_name + "/" +output+str(i+1)+"."+extension
 
-		subprocess.run(cmd, shell=True)
-		print(cmd)
 
 input_file = "lowBridge.mp4"
 input_stream = ffmpeg.input(input_file)

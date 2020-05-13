5ae3838146e80624f15838928a4f2f3eea812d31
Vasu Gupta <guptavasu1213@gmail.com>
2020-05-13 02:00:38 -0600

    FPS reduction implemented

--- intro-ffmpeg.py
+++ intro-ffmpeg.py
@@ -1,5 +1,4 @@
 import subprocess
-import ffmpeg
 from cv2 import VideoCapture, CAP_PROP_FRAME_COUNT, CAP_PROP_FPS
 
 def remove_audio(input_file, output_file):
@@ -13,8 +12,14 @@ def remove_audio(input_file, output_file):
 	subprocess.run(cmd, shell=True)
 	print(cmd)
 
-def reduceFPS():
-	pass
+def reduceFPS(input_file, output_file, new_fps):
+	"""
+	:param input_file: input file name
+	:param output_file: output file name
+	"""
+	cmd = "ffmpeg -i " + input_file + " -r " + str(new_fps) + " " + output_file	
+	subprocess.run(cmd, shell=True)
+	print(cmd)
 
 def decreaseBitRate():
 	pass
@@ -37,10 +42,9 @@ def get_video_length(input_file):
 	return float(total_frames) / float(fps)
 
 
-def split_video_into_chunks(input_stream, input_file, output_file, seconds):
+def split_video_into_chunks(input_file, output_file, seconds):
 	"""
 	Splits the input stream into multiple files with re-encoding
-	:param input_stream: input file stream
 	:param input_file: input file name
 	:param output_file: output file name
 	:param seconds: the duration of each splitted video (in seconds)
@@ -53,17 +57,6 @@ def split_video_into_chunks(input_stream, input_file, output_file, seconds):
 	length_in_seconds = get_video_length(input_file)
 	total_videos_to_create = (int(length_in_seconds)//seconds) + 1
 
-	# cmd = "ffmpeg -ss " + str(start_seconds) + " -i " + input_file + " -c copy"+ " -t " + str(length_in_seconds-start_seconds) + \
-	# " " + folder_name + "/" +output+str(i+1)+"."+extension
-	# subprocess.run(cmd, shell=True)
-	# print(cmd)
-	
-	#############Works but inaccurate
-	#ffmpeg -i bridge.mp4 -ss 20 -t 4.0300375469336664 -c copy bridge/output_file5.mp4
-	# # cmd = "ffmpeg -i " + input_file + " -ss " + str(start_seconds) + " -t " + str(length_in_seconds-start_seconds) + \
-	# # " -c:v copy -an " + folder_name + "/" +output+str(i+1)+"."+extension
-	# subprocess.run(cmd, shell=True)
-	# print(cmd)
 	for i in range(total_videos_to_create-1):
 		start_seconds = (i*seconds)
 
@@ -80,37 +73,9 @@ def split_video_into_chunks(input_stream, input_file, output_file, seconds):
 	subprocess.run(cmd, shell=True)
 	print(cmd)
 
-		# OTHER WAY
-		# (
-		# 	input_stream
-		# 		.filter("trim", start=start_seconds, duration=seconds)
-		# 		.setpts ('PTS-STARTPTS') # resets the timestamp to 0 after a trim filter has been passed
-		# 		.output(folder_name+str(i+1)+extension)
-		# 		.run()
-		# )
-		# Example: ffmpeg -ss 0 -i bridge.mp4 -t 5 -vcodec copy bridge/output_file1.mp4
 
-		##########Not working
-		# cmd = "ffmpeg -ss " + str(start_seconds) + " -i " + input_file + " -t " + str(seconds) + \
-		# " -vcodec copy " + folder_name + "/" +output+str(i+1)+"."+extension
+# input_file = "lowBridge.mp4"
 
-		# NOt working with the same encoding
-		# cmd = "ffmpeg -i " + input_file + " -ss " + str(start_seconds) + " -to " + str(start_seconds+seconds) + \
-		# " -vcodec copy -an " + folder_name + "/" +output+str(i+1)+"."+extension
-
-
-input_file = "lowBridge.mp4"
-input_stream = ffmpeg.input(input_file)
-# print(length_in_seconds)
-split_video_into_chunks(input_stream, input_file, "output_file.mp4", 5)
-# inputStream.filter('fps', fps=1, round='up').output(outputFile).run()
-remove_audio(input_file, "silentFile.mp4")
-# stream = ffmpeg.input('in.mp4')
-# stream = ffmpeg.zoompan(stream, fps=10)
-# stream = ffmpeg.output(stream, 'out.mp4')
-# ffmpeg.run(stream)
-#
-# input_stream = ffmpeg.input('bridge.mp4')
-# ffmpeg.input('bridge.mp4')
-
-# .output('output.mp4').run()
+# split_video_into_chunks(input_file, "output_file.mp4", 5)
+# remove_audio(input_file, "silentFile.mp4")
+reduceFPS("sd",'sddsds',12)

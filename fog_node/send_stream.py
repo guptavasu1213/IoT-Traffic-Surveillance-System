067ccc80c32547d03157c353ee70ca8e351976dd
Vasu Gupta <guptavasu1213@gmail.com>
2020-09-13 00:30:15 -0600

    :sparkles: Split original videos in segments

---
+++ fog_node/send_stream.py
@@ -0,0 +1,29 @@
+from client import *
+import os
+import argparse
+
+# Parsing the arguments to retrieve the camera and the fog name
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--camera_name", help="Name of camera", required=True)
+ap.add_argument("-f", "--fog_name", help="Name of fog", required=True)
+ap.add_argument("-ip", "--ip_address", help="IP Address of the server", required=True)
+ap.add_argument("-pn", "--port_number", help="Port Number of the server", required=True, type=int)
+ap.add_argument("-ei", "--encoding_interval", help="Time interval for encoding calculation (in minutes)", required=False, default=60, type=int)
+args = vars(ap.parse_args())
+
+#Get the camera name from command line
+cam_name = args["camera_name"]
+fog_node_name = args["fog_name"]
+
+#Error check for the existence of camera folder
+if cam_name not in os.listdir("./surveillance-cam-videos"):
+	print("Camera folder not found")
+	exit(1)
+
+#Error check for the existence of camera videos in its folder
+if len(os.listdir("./surveillance-cam-videos/"+cam_name)) == 0:
+	print("No camera files in the folder")
+	exit(1)
+
+# Estabilish a connection with the server for the given fog and camera
+client(fog_node_name, cam_name, args["ip_address"], args["port_number"], args["encoding_interval"]*60)
---
+++ fog_node/split_video_into_chunks.py
@@ -0,0 +1,58 @@
+import subprocess
+from cv2 import VideoCapture, CAP_PROP_FRAME_COUNT, CAP_PROP_FPS
+import argparse
+
+# Parsing the arguments to retrieve the camera and the fog name
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input_file_path", help="File path of the input video file", required=True)
+ap.add_argument("-f", "--camera_folder_name", help="Camera folder name (Unique)", required=True)
+ap.add_argument("-d", "--chunk_duration", help="Duration of each video segment", required=False, 
+	default=1.0, type=float)
+args = vars(ap.parse_args())
+
+def get_video_length(input_file):
+	"""
+	:param input_file:
+	:return: Total video length in seconds
+	"""
+	video = VideoCapture(input_file)
+	fps = video.get(CAP_PROP_FPS)
+	total_frames = video.get(CAP_PROP_FRAME_COUNT)
+	return float(total_frames) / float(fps)
+
+def main(input_file, camera_folder_name, seconds):
+	"""
+	Splits the input stream into multiple files with re-encoding
+	:param input_file: input file path
+	:param camera_folder_name: name of the folder which will contain the splitted camera videos
+	:param seconds: the duration of each splitted video (in seconds)
+	"""
+	folder_path = "./surveillance-cam-videos/{}".format(camera_folder_name)
+	extension = input_file[input_file.rfind(".")+1 : ]
+
+	# Creating a folder for storing streamed videos	
+	subprocess.run("mkdir {}".format(folder_path) , shell=True) 
+		
+	length_in_seconds = get_video_length(input_file)
+	print(length_in_seconds)
+	total_videos_to_create = int(int(length_in_seconds)//seconds)
+
+	for i in range(total_videos_to_create):
+		start_seconds = (i*seconds)
+
+		# ffmpeg -ss <starting point> -i <input file> -t <duration of the clip> <output file>
+		cmd = "ffmpeg -ss {} -i {} -t {} -y {}/{}.{} 2>/dev/null".format(str(start_seconds), input_file, 
+			str(seconds), folder_path, str(i), extension)
+
+		subprocess.run(cmd, shell=True)
+		print(cmd)
+
+	# For the last video:
+	start_seconds = (total_videos_to_create) * seconds
+	cmd = "ffmpeg -ss {} -i {} -t {} -y {}/{}.{} 2>/dev/null".format(str(start_seconds), input_file, 
+		str(length_in_seconds-start_seconds), folder_path, str(total_videos_to_create), extension)
+
+	subprocess.run(cmd, shell=True)
+	print(cmd)
+
+main(args["input_file_path"], args["camera_folder_name"], args["chunk_duration"])
